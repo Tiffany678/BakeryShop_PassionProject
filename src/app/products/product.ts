b7d7124cb60a44316c9ec238cfbf861466227c35
Yingjie <52Tiffany.yang@gmail.com>
2023-11-03 16:38:00 -0400

    Adding the Front end

---
+++ src/app/products/product.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number,
+    title: string,
+    price: number,
+    limitedNum: number,
+    information: string,
+    starRating: number,
+    imageUrl: string;
+}
