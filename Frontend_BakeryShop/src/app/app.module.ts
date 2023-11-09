ae3eda8e3da80fe1da3287a25f97b5df4e844902
Yingjie <52Tiffany.yang@gmail.com>
2023-11-09 17:53:11 -0500

    add route

--- Frontend_BakeryShop/src/app/app.module.ts
+++ Frontend_BakeryShop/src/app/app.module.ts
@@ -4,16 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponet } from './products/product-list.components';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponet
+    WelcomeComponent,
+    ProductListComponet,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'products', component: ProductListComponet },
+      {path:'products/:id', component: ProductDetailComponent },
+      {path:'welcome', component: WelcomeComponent },
+      {path:'', redirectTo: 'welcome', pathMatch: 'full' },
+      {path:'**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [AppComponent]
 })
